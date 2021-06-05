b5605a99659488f4aa81c442ed9b5ad03456ad00
booiris <954520770@qq.com>
2021-06-05 16:57:07 +0800

    update

--- src/bit/minisys/minicc/BITMiniCC.java
+++ src/bit/minisys/minicc/BITMiniCC.java
@@ -1,30 +1,30 @@
 package bit.minisys.minicc;
 
 public class BITMiniCC {
-	
+
 	/**
 	 * @param args
-	 * @throws Exception 
+	 * @throws Exception
 	 */
-	public static void main(String[] args) throws Exception {		
-		if(args.length < 1){
+	public static void main(String[] args) throws Exception {
+		if (args.length < 1) {
 			usage();
 			return;
 		}
-		
+
 		String file = args[0];
-		if(!file.endsWith(".c")){
+		if (!file.endsWith(".c")) {
 			System.out.println("Incorrect input file:" + file);
 			return;
 		}
-		
+
 		MiniCCompiler cc = new MiniCCompiler();
 		System.out.println("Start to compile ...");
 		cc.run(file);
 		System.out.println("Compiling completed!");
 	}
-	
-	public static void usage(){
+
+	public static void usage() {
 		System.out.println("USAGE: BITMiniCC FILE_NAME.c");
 	}
 }
