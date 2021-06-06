4eae5b92c2946784716a58a29cc17017183e957c
booiris <954520770@qq.com>
2021-06-06 10:38:21 +0800

    update

--- src/bit/minisys/minicc/ncgen/myCodeGen.java
+++ src/bit/minisys/minicc/ncgen/myCodeGen.java
@@ -10,28 +10,31 @@ import bit.minisys.minicc.icgen.internal.MiniCCICGen;
 import bit.minisys.minicc.internal.util.MiniCCUtil;
 import bit.minisys.minicc.ncgen.IMiniCCCodeGen;
 
+public class myCodeGen implements IMiniCCCodeGen {
 
-public class ExampleCodeGen implements IMiniCCCodeGen{
+	public myCodeGen() {
 
-
-	public ExampleCodeGen() {
-		
 	}
-	
+
 	@Override
 	public String run(String iFile, MiniCCCfg cfg) throws Exception {
 		String oFile = MiniCCUtil.remove2Ext(iFile) + MiniCCCfg.MINICC_CODEGEN_OUTPUT_EXT;
 
-		if(cfg.target.equals("mips")) {
-			//TODO:
-		}else if (cfg.target.equals("riscv")) {
-			//TODO:
-		}else if (cfg.target.equals("x86")){
-			//TODO:
+		if (cfg.target.equals("mips")) {
+			System.out.println("123");
+			System.out.println("1231233333");
+			System.out.println("123333");
+			for (int x = 0; x < 10; x++) {
+				
+			}
+		} else if (cfg.target.equals("riscv")) {
+			// TODO:
+		} else if (cfg.target.equals("x86")) {
+			// TODO:
 		}
 
 		System.out.println("7. Target code generation finished!");
-		
+
 		return oFile;
 	}
 }
